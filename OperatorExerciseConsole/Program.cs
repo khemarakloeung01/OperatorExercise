3887fba973cfab3bf6d5f2117a91019e5f083f78
Khemarak Loeung <khemarakloeung01@gmail.com>
2020-10-13 16:17:41 -0500

    Initial Commit

---
+++ OperatorExerciseConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorExerciseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            var r = 20;
+            var pi = Math.PI;
+            var areaofCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with a radius of {r} is {areaofCircle}");
+
+        }
+    }
+
+}
